b3a7af393af512fac84183200dcfc3c3dc561ed1
Michelle Amina Okumu <michelleaokumu@gmail.com>
2023-12-19 11:42:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+
+function superbowlWin(record){
+    const winning = record.find((object) => object.result === "W");
+  
+  // If a win is found, (?) return the year the win is found in,(:) otherwise, return undefined
+   return winning? winning.year : undefined;
+  } 
+
+  // used this to confirm the code 
+  console.log(superbowlWin(record)); //2015
+
+
+ 
+  
+  
+  
